18f0ca4d601ffcf3a08e2f3497e0ff95542c5be6
kevinmenezes <kevinmenezes@yahoo.com>
2020-01-28 11:19:15 -0800

    added post_sensor and post_traffic services

--- api/controllers/mainController.js
+++ api/controllers/mainController.js
@@ -3,6 +3,7 @@ var logger = log4js.getLogger();
 var con = require('../common/database.js');
 logger.level = 'debug';
 
+// gets the latest Sensor reading. When a person activates the sensor, this request is called
 exports.get_sensor_reading = function(req, res){
     res.setHeader('Access-Control-Allow-Origin','*');
 
@@ -13,6 +14,7 @@ exports.get_sensor_reading = function(req, res){
     });
 }
 
+// updates Sensor table to set the flag for the sensor that was just activated
 exports.post_sensor_reading = function(req, res){
     res.setHeader('Access-Control-Allow-Origin','*');
 
@@ -20,14 +22,35 @@ exports.post_sensor_reading = function(req, res){
     datelocal = (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() -
         ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ');
 
-    if (sensorId.toUpperCase() == 'A')
+    if (sensorId.toUpperCase() == 'A') {
+        sql = "insert into Sensor (sensorAFlag, sensorATimestamp) values (1, '" + datelocal + "')" ;
+    }
+    if (sensorId.toUpperCase() == 'B') {
         sql = "insert into Sensor (sensorBFlag, sensorBTimestamp) values (1, '" + datelocal + "')";
-    else
-        sql = "insert into Sensor (sensorAFlag, sensorATimestamp) values (1, '" + datelocal + "')";
+    }
 
     con.query(sql, function  (err, result) {
         if (err) throw err;
-        res.send('Sensor ' + sensorId.toUpperCase() + ' Updated');
+        res.send('Sensor: ' + sensorId.toUpperCase() + ' Updated');
     });
+}
 
+// updates Traffic table to signify an In or Out activity
+exports.post_traffic_reading = function(req, res){
+    res.setHeader('Access-Control-Allow-Origin','*');
+
+    directionFlag = req.query.id;
+    datelocal = (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() -
+        ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ');
+
+    if (directionFlag == 'in')
+        sql = "insert into Traffic (inCount, inTimestamp) values (1, '" + datelocal + "')";
+
+    if (directionFlag == 'out')
+        sql = "insert into Traffic (outCount, outTimestamp) values (1, '" + datelocal + "')";
+
+    con.query(sql, function  (err, result) {
+        if (err) throw err;
+        res.send('Activity: ' + directionFlag.toUpperCase() + ' Updated');
+    });
 }
