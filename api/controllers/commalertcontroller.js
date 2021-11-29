337fa4c472d95b5ac9cff872b74536ded0999b60
Kevin Menezes <kevinmenezes@yahoo.com>
2021-11-29 09:13:38 -0800

    added comm_alert and get route

---
+++ api/controllers/commalertcontroller.js
@@ -0,0 +1,14 @@
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+var con = require('../common/database.js');
+
+logger.level = 'debug';
+
+exports.get_alert_reading = function(req, res){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    var sql = "select comm_alert_id, alert, DATE_FORMAT(timestamp, GET_FORMAT(DATETIME, 'USA')) as alert_dt from comm_alert order by timestamp desc limit 1";
+    con.query(sql, function(err, result){    
+        if(err) throw err;      
+        res.send(result);
+    })   
+}
--- api/routes/mainRoutes.js
+++ api/routes/mainRoutes.js
@@ -1,15 +1,16 @@
 module.exports = function(app){
     var main = require('../controllers/mainController.js');
     var waterflow = require('../controllers/waterflowcontroller');
-
+    var comm_alert = require('../controllers/commalertcontroller');
+    
     // app.route('/sensor')
     //     .get(main.get_sensor_reading)
     //     .post(main.post_sensor_reading);
-    //
+    
     // app.route('/traffic')
     //     .post(main.post_traffic_reading);
 
-    // twilio-hack-coronavirus project Apr, 2020
+    // //twilio-hack-coronavirus project Apr, 2020
     // app.route('/items')
     //     .get(main.get_items_needed)
     //     .post(main.post_items_needed);
@@ -21,4 +22,8 @@ module.exports = function(app){
     app.route('/waterflow')
         .get(waterflow.get_sensor_readings)
         .post(waterflow.post_sensor_reading);    
+    
+    // comm_alert readings
+    app.route('/alert')
+        .get(comm_alert.get_alert_reading);
 }
